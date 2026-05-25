8cb22713a102e35f0c0a98c770ff2b5a8dc39507
Aikantic Maitra <maitraaikantic554@gmail.com>
2026-05-25 15:46:25 +0530

    Feat: Multi-variable weather support and Uvicorn ASGI app startup fix with requirements.txt

--- main.py
+++ main.py
@@ -296,4 +296,4 @@ async def get_frontend():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main.py", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
